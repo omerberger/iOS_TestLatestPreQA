c3bad0f4cc41cf8b806bdbd3b35b9fd21bbd9f06
Omer Berger <oberger@liveperson.com>
2016-08-10 11:18:59 -0400

    Initial Commit

---
+++ TestLatestPreQA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestLatestPreQA
+//
+//  Created by Omer Berger on 8/10/16.
+//  Copyright © 2016 LivePerson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestLatestPreQA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestLatestPreQA
+//
+//  Created by Omer Berger on 8/10/16.
+//  Copyright © 2016 LivePerson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
